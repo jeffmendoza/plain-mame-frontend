e6ce7d1e2f969bf1d54a82b238b2c77c6cfe1563
Jeff Mendoza <jefflmendoza@gmail.com>
2010-12-16 21:28:57 -0600

    deb updates

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 
-setup(name='plain-mame-frontend',
+name='plain-mame-frontend'
+
+setup(name=name,
       version='0.1',
       description='Plain MAME Frontend',
       author='Jeff Mendoza',
@@ -9,6 +11,8 @@ setup(name='plain-mame-frontend',
       requires=['libxml2', 'curses', 'sys',],
       scripts=['bindir/pmfe',],
       license='GPL',
+      data_files=[("share/%s" % name, ['datadir/pmfe_cli.py',  'datadir/pmfe_tui.py']),
+                  ("/etc/%s" % name, ['sysconfdir/pmfe.ini'])],
       long_description="""Plain MAME Frontend is a basic, easy to use frontend to mame.
 It works out of the box with no config, and provides menus customized to the set of games you have, not the whole mame set.
 PMFE is a console program used with the keyboard.
